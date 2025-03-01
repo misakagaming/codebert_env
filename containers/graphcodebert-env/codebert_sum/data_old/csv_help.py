ac3c81784fc3d07e518de520d4766109291af061
Eray Erer <eray.erer@hotmail.com>
2025-03-02 00:45:30 +0300

    help

--- containers/graphcodebert-env/codebert_sum/data_old/csv_help.py
+++ containers/graphcodebert-env/codebert_sum/data_old/csv_help.py
@@ -3,8 +3,23 @@ import pandas as pd
 
 
 
-for file_path in ['test.csv', 'train.csv', 'valid.csv']:
+"""for file_path in ['test.csv', 'train.csv', 'valid.csv']:
     df = pd.read_csv(file_path)
-    for index, row in df.iterrows():
-        df["code"][index] = df["code"][index].replace("\n",' ').replace("\t", ' ')
-    df.to_csv(file_path, index=False) 
+    print(df.head(30))
+    df1 = df["code"]
+    df1 = df1.replace(r'\n',' ', regex=True)
+    df1 = df1.replace(r'\t',' ', regex=True)
+    df1 = df1.replace(r'\s', ' ', regex = True)
+    df["code"] = df1
+    print(df.head(30))    
+    df.to_csv(file_path, index=False)"""
+
+
+for file in ['test.csv', 'train.csv', 'valid.csv']:
+    with open(file, encoding="utf-8") as f:
+        data = f.readlines()
+    for i in range(len(data)):
+        if data[i][0] == '"':
+            data[i] = str(data[i][1:-2] + "\n")
+    with open(file, 'w', encoding="utf-8") as file:
+        file.writelines( data )    
