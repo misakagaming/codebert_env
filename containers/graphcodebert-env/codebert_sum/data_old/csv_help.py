26ceef7029e9c46bd952fa29c1e6558d731a65e8
Eray Erer <eray.erer@hotmail.com>
2025-03-02 15:05:54 +0300

    help

--- containers/graphcodebert-env/codebert_sum/data_old/csv_help.py
+++ containers/graphcodebert-env/codebert_sum/data_old/csv_help.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 
-"""for file_path in ['test.csv', 'train.csv', 'valid.csv']:
+for file_path in ['test.csv', 'train.csv', 'valid.csv']:
     df = pd.read_csv(file_path)
     print(df.head(30))
     df1 = df["code"]
@@ -12,14 +12,6 @@ import pandas as pd
     df1 = df1.replace(r'\s', ' ', regex = True)
     df["code"] = df1
     print(df.head(30))    
-    df.to_csv(file_path, index=False)"""
+    df.to_csv(file_path, index=False)
 
 
-for file in ['test.csv', 'train.csv', 'valid.csv']:
-    with open(file, encoding="utf-8") as f:
-        data = f.readlines()
-    for i in range(len(data)):
-        if data[i][0] == '"':
-            data[i] = str(data[i][1:-2] + "\n")
-    with open(file, 'w', encoding="utf-8") as file:
-        file.writelines( data )    
